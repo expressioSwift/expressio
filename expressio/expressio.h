14c29ee9ad181ab8a0b360092806d1e3b1b7e8c8
Zbigniew Górawski <zgorawski@gmail.com>
2018-08-06 15:19:23 +0200

    Initial Commit

---
+++ expressio/expressio.h
@@ -0,0 +1,19 @@
+//
+//  expressio.h
+//  expressio
+//
+//  Created by Zbigniew Górawski on 06/08/2018.
+//  Copyright © 2018 expressioSwift. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for expressio.
+FOUNDATION_EXPORT double expressioVersionNumber;
+
+//! Project version string for expressio.
+FOUNDATION_EXPORT const unsigned char expressioVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <expressio/PublicHeader.h>
+
+
